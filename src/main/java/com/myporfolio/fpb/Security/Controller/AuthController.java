89712a47506e5afec48cd31413a984d72c66d896
Julian <julypuebla@gmail.com>
2022-08-15 14:25:50 -0300

    corrigiendo errores

--- src/main/java/com/myporfolio/fpb/Security/Controller/AuthController.java
+++ src/main/java/com/myporfolio/fpb/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://frontendfpb.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
